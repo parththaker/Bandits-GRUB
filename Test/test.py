efee8e11187eb399d69ccc1b73410e45191e5695
parththaker <parneh@gmail.com>
2021-04-17 23:24:47 -0700

    Adding unittest base

---
+++ Test/test.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.append('../')
+
+import graph_algo
+import support_func
+import unittest
+
+
+class TestGraphAlgo(unittest.TestCase):
+
+    def test_one(self):
+        self.assertEqual(1, 1)
+
+
+if __name__ == "__main__":
+
+    unittest.main()
